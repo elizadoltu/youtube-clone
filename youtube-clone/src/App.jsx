8c891348274ea3cc7590db5c48288d04da2e3f1a
unknown <elizadoltuofficial@gmail.com>
2024-04-11 12:22:05 +0300

    add:initial files

---
+++ youtube-clone/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/home/home"
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world! 
+    </h1>
+    <Home />
+    </div>
+  )
+}
---
+++ youtube-clone/src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Home 
+      </h1>
+    </div>
+  )
+}
